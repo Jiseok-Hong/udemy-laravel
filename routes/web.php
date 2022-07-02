9874ff1c3887a1020e5d81cf795040df22f92eca
Jiseok-Hong <johnsk75252@gmail.com>
2022-07-02 16:13:15 +0800

    Test nested nested blade view template

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return '<h1>homepage</h1>';
+})->name('base.index');
+
 
 Route::get('/main', function () {
     return view('welcome');
-});
+})->name('main.index');
 
 
-Route::get('/', function () {
-    return 'homepage';
-});
+Route::get('/home', function () {
+    return view('home.index');
+})->name('home.index');
+
+
+Route::get('/blog/{id}/{random?}', function ($blogId, $ageId = 0) {
+    return 'homepage ' . $blogId . $ageId;
+})->name('home.index');
